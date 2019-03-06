ad53fc7e2eaeea657074497108701f4a34d98a99
Robert Malko <rmalko@doximity.com>
2019-03-06 14:49:12 -0500

    Small refactoring

--- src/simple_db/cli.cr
+++ src/simple_db/cli.cr
@@ -5,16 +5,24 @@ class SimpleDb::CLI
   def start
     while true
       prompt
-      command = gets.to_s.strip
+      command = read_input
       if command == ".exit"
         exit 0
       else
-        puts "Unrecognized command #{command}"
+        unrecognized_command_message(command: command)
       end
     end
   end
 
   private def prompt
     print "db > "
+  end
+
+  private def read_input
+    gets.to_s.strip
+  end
+
+  private def unrecognized_command_message(command)
+    puts "Unrecognized command #{command}"
   end
 end
