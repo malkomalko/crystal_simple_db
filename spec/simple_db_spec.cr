3412c4fd32cf8df931c9ffb129da19e583432c72
Robert Malko <rmalko@doximity.com>
2019-03-06 14:06:32 -0500

    Remove some comments and update README

--- spec/simple_db_spec.cr
+++ spec/simple_db_spec.cr
@@ -1,8 +1,6 @@
 require "./spec_helper"
 
 describe SimpleDb do
-  # TODO: Write tests
-
   it "works" do
     false.should eq(true)
   end
--- src/simple_db.cr
+++ src/simple_db.cr
@@ -1,6 +1,3 @@
-# TODO: Write documentation for `SimpleDb`
 module SimpleDb
   VERSION = "0.1.0"
-
-  # TODO: Put your code here
 end
